89ddae9a6c7244197c0c88317f0e970105ccc2b9
12Garfield <garfieldtanakrit@gmail.com>
2024-12-04 16:06:24 +0700

    Add files via upload

---
+++ 1form_12garfield.py
@@ -0,0 +1,11 @@
+print("Welcome to Pre-Engineering & Robotics")
+
+print("\n\กรุณากรอกข้อมูลตนเอง")
+name = input ("ชื่อ-นามสกุล")
+age = input ("อายุ")
+birth = input ("วันเกิด")
+
+
+print("\n\ยินดีต้อนรับครับ", name , "ชื่อเหมือนลิงเลยครับ")
+print("อายุ ", age, "\nวันเกิด", birth)
+print("\n\แล้วพบกันใหม่นะฮาฟฟู่ว")
